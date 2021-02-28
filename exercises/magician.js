a94624913ac19d911bddb4995edf1d9434396424
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-28 10:55:18 -0700

    Complete Magician

--- exercises/magician.js
+++ exercises/magician.js
@@ -8,10 +8,28 @@ class Magician  {
     } else {
       this.favoriteAccessory = obj.clothing;
     }
+    this.confidencePercentage = 10;
   }
 
   performIncantation(incantation)  {
     return `${incantation.toUpperCase()}!`
+  }
+
+  performTrick()  {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === 'top hat') {
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
+    }
+  }
+
+  performShowStopper()  {
+    if (this.confidencePercentage >= 100 && this.assistant === true)  {
+      return 'WOW! The magician totally just sawed that person in half!';
+    } else {
+      return 'Oh no, this trick is not ready!';
+    }
   }
 }
 
