4fb1ae085c8f858026fd52e1d08851c9790416e7
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-05 12:17:27 -0700

    Sphinx collects riddles

--- exercises/sphinx.js
+++ exercises/sphinx.js
@@ -2,6 +2,10 @@ class Sphinx  {
   constructor() {
     this.riddles = []
   }
+
+  collectRiddle(riddle) {
+    this.riddles.push(riddle);
+  }
 }
 
 module.exports = Sphinx;
--- test/sphinx-test.js
+++ test/sphinx-test.js
@@ -21,7 +21,7 @@ describe('Sphinx', () => {
     assert.deepEqual(sphinx.riddles, []);
   });
 
-  it.skip('should collect riddles', () => {
+  it('should collect riddles', () => {
     const sphinx = new Sphinx();
     const riddle = {
       riddle: 'What word becomes shorter when you add two letters to it?',
