549a8f1d08eaaf0cb3d98564cda0a82a88b709bd
Michael Alex Lynch <mlynch5187@gmail.com>
2021-07-11 11:12:24 -0600

    Mermaids test to collect pearls

--- test/mermaid-test.js
+++ test/mermaid-test.js
@@ -1,5 +1,7 @@
 var assert = require('chai').assert;
 var Mermaid = require('../exercises/mermaid');
+var Pearl = require('../exercises/pearl');
+
 
 describe('Mermaid', function() {
 
@@ -17,5 +19,32 @@ describe('Mermaid', function() {
     var mermaid = new Mermaid('Ariel');
 
     assert.equal(mermaid.name, 'Ariel');
+  });
+
+  it('should start with a no pearls', function() {
+    var mermaid = new Mermaid('Ariel');
+
+    assert.deepEqual(mermaid.pearls, []);
+  });
+
+  it('should be able to collect pearls', function() {
+    var mermaid = new Mermaid('Ariel');
+    var red = new Pearl('Red Pearl')
+    var blue = new Pearl('Blue Pearl')
+    var violet = new Pearl('Violet Pearl')
+
+    mermaid.collectPearl(red);
+    mermaid.collectPearl(blue);
+    mermaid.collectPearl(violet);
+
+    assert.equal(mermaid.pearls.length, 3);
+    assert.instanceOf(mermaid.pearls[0], Pearl);
+    assert.instanceOf(mermaid.pearls[1], Pearl);
+    assert.instanceOf(mermaid.pearls[2], Pearl);
+
+    assert.equal(mermaid.pearls[0].name, 'Red Pearl');
+    assert.equal(medusa.pearls[1].name, 'Blue Pearl');
+    assert.equal(medusa.pearls[1].name, 'Violet Pearl');
+    assert.deepEqual(mermaid.pearls, [red, blue, violet]);
   });
 });
