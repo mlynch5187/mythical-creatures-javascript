a1fbf29ea44b12cc44fa1c774f28da82456353f4
Michael Alex Lynch <mlynch5187@gmail.com>
2021-07-11 10:57:58 -0600

    Mermaid tests for function, instantiation, and name

--- test/mermaid-test.js
+++ test/mermaid-test.js
@@ -3,5 +3,19 @@ var Mermaid = require('../exercises/mermaid');
 
 describe('Mermaid', function() {
 
+  it('should be a function', function() {
+    assert.isFunction(Mermaid);
+  });
 
+  it('should instantiate our Mermaid', function() {
+    var mermaid = new Mermaid();
+
+    assert.instanceOf(mermaid, Mermaid);
+  });
+
+  it('should have a name', function() {
+    var mermaid = new Mermaid('Ariel');
+
+    assert.equal(mermaid.name, 'Ariel');
+  });
 });
